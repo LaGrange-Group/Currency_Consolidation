f42de67b7d42f78675d91edd54a303cf819583c0
David <davidnlagrange@hotmail.com>
2018-11-17 20:17:56 -0600

    Added run Function to Call When Created

--- Consolidation.py
+++
@@ -1,6 +0,0 @@
-import MySQLdb, telegram
-
-conn=MySQLdb.connect(host='localhost', user='root',passwd='###', db='datadb')
-cursor = conn.cursor()
-
-class Consolidation:
--- Python File/Consolidation.py
+++ Python File/Consolidation.py
@@ -3,81 +3,83 @@ import MySQLdb, telegram
 conn=MySQLdb.connect(host='localhost', user='root',passwd='###', db='datadb')
 cursor = conn.cursor()
 
-class Consolidation:
+class Consolidation_Binance:
 
-    def scan_for_consolidation():
-        consolidation_scan_query = """INSERT INTO consolidation (TOKEN, BATCH_KEY)
-                                        SELECT A.TICKER AS TOKEN
-                                        	   ,D.CURRENT_KEY
+    def run():
+
+        def scan_for_consolidation():
+            consolidation_scan_query = """INSERT INTO consolidation (TOKEN, BATCH_KEY)
+                                            SELECT A.TICKER AS TOKEN
+                                            	   ,D.CURRENT_KEY
 
 
-                                        	FROM
-                                        		(SELECT TICKER, PRICE, BATCH_KEY
-                                        		FROM storage
-                                        		WHERE BATCH_KEY = (SELECT (MAX(BATCH_KEY) - 5060) FROM batch)) A
+                                            	FROM
+                                            		(SELECT TICKER, PRICE, BATCH_KEY
+                                            		FROM storage
+                                            		WHERE BATCH_KEY = (SELECT (MAX(BATCH_KEY) - 5060) FROM batch)) A
 
-                                        		INNER JOIN (SELECT TICKER, MAX(PRICE) AS MAX_PRICE
-                                        		FROM storage
-                                                WHERE BATCH_KEY BETWEEN (SELECT (MAX(BATCH_KEY) - 5060) FROM batch) AND (SELECT MAX(BATCH_KEY) FROM batch)
-                                        		GROUP BY TICKER) B
-                                        		ON A.TICKER = B.TICKER
+                                            		INNER JOIN (SELECT TICKER, MAX(PRICE) AS MAX_PRICE
+                                            		FROM storage
+                                                    WHERE BATCH_KEY BETWEEN (SELECT (MAX(BATCH_KEY) - 5060) FROM batch) AND (SELECT MAX(BATCH_KEY) FROM batch)
+                                            		GROUP BY TICKER) B
+                                            		ON A.TICKER = B.TICKER
 
-                                        		INNER JOIN (SELECT TICKER, MIN(PRICE) AS MIN_PRICE
-                                        		FROM storage
-                                                WHERE BATCH_KEY BETWEEN (SELECT (MAX(BATCH_KEY) - 5060) FROM batch) AND (SELECT MAX(BATCH_KEY) FROM batch)
-                                        		GROUP BY TICKER) C
-                                        		ON A.TICKER = C.TICKER
+                                            		INNER JOIN (SELECT TICKER, MIN(PRICE) AS MIN_PRICE
+                                            		FROM storage
+                                                    WHERE BATCH_KEY BETWEEN (SELECT (MAX(BATCH_KEY) - 5060) FROM batch) AND (SELECT MAX(BATCH_KEY) FROM batch)
+                                            		GROUP BY TICKER) C
+                                            		ON A.TICKER = C.TICKER
 
-                                        		INNER JOIN (SELECT MAX(BATCH_KEY) AS CURRENT_KEY
-                                        		FROM batch) D
+                                            		INNER JOIN (SELECT MAX(BATCH_KEY) AS CURRENT_KEY
+                                            		FROM batch) D
 
-                                        WHERE B.MAX_PRICE < (A.PRICE * 0.015 + A.PRICE)
-                                        AND C.MIN_PRICE > ((A.PRICE * 0.015 + (A.PRICE * -1)) * -1)
-                                        AND A.TICKER NOT IN (SELECT TOKEN FROM consolidation)
-                                        AND A.TICKER NOT IN ('BCCBTC')"""
-        cursor.execute(consolidation_scan_query)
-        conn.commit()
+                                            WHERE B.MAX_PRICE < (A.PRICE * 0.015 + A.PRICE)
+                                            AND C.MIN_PRICE > ((A.PRICE * 0.015 + (A.PRICE * -1)) * -1)
+                                            AND A.TICKER NOT IN (SELECT TOKEN FROM consolidation)
+                                            AND A.TICKER NOT IN ('BCCBTC')"""
+            cursor.execute(consolidation_scan_query)
+            conn.commit()
 
-    def send_tele_sig(msg):
-    	"""
-    	Send a mensage to a telegram user specified on chatId
-    	chat_id must be a number!
-    	"""
-    	bot = telegram.Bot(token='###')
-    	bot.sendMessage(chat_id='@consolidationbinance', text=msg)
+        def send_tele_sig(msg):
+        	"""
+        	Send a mensage to a telegram user specified on chatId
+        	chat_id must be a number!
+        	"""
+        	bot = telegram.Bot(token='###')
+        	bot.sendMessage(chat_id='@consolidationbinance', text=msg)
 
-# Truncate daydoji table
-    def truncate_three_day_doji_table():
-        cursor.execute("TRUNCATE TABLE consolidation")
-        conn.commit()
+    # Truncate daydoji table
+        def truncate_three_day_doji_table():
+            cursor.execute("TRUNCATE TABLE consolidation")
+            conn.commit()
 
-    # Get highest BATCH_KEY
-    def set_max_consolidation_batch():
-        cursor.execute("SELECT MAX(BATCH_KEY) AS maximum FROM consolidation")
-        result = cursor.fetchall()
-        for i in result:
-            batch_id_fn = (i[0])
-        return batch_id_fn
+        # Get highest BATCH_KEY
+        def set_max_consolidation_batch():
+            cursor.execute("SELECT MAX(BATCH_KEY) AS maximum FROM consolidation")
+            result = cursor.fetchall()
+            for i in result:
+                batch_id_fn = (i[0])
+            return batch_id_fn
 
-    # Set python variable equal to the highest BATCH_KEY column value in batches table
-    def set_new_batchid():
-        cursor.execute("SELECT MAX(BATCH_KEY) AS maximum FROM batch")
-        result = cursor.fetchall()
-        for i in result:
-            batch_id_fn = (i[0])
-        return batch_id_fn
+        # Set python variable equal to the highest BATCH_KEY column value in batches table
+        def set_new_batchid():
+            cursor.execute("SELECT MAX(BATCH_KEY) AS maximum FROM batch")
+            result = cursor.fetchall()
+            for i in result:
+                batch_id_fn = (i[0])
+            return batch_id_fn
 
-# Get all results that have highest BATCH_KEY
-    def collect_new_results_table():
-        cursor.execute("SELECT TOKEN FROM consolidation WHERE BATCH_KEY = (SELECT MAX(BATCH_KEY) FROM consolidation)")
-        result = cursor.fetchall()
-        results_of_table = result
-        return results_of_table
+    # Get all results that have highest BATCH_KEY
+        def collect_new_results_table():
+            cursor.execute("SELECT TOKEN FROM consolidation WHERE BATCH_KEY = (SELECT MAX(BATCH_KEY) FROM consolidation)")
+            result = cursor.fetchall()
+            results_of_table = result
+            return results_of_table
 
-    scan_for_consolidation()
-    consolidation_key = set_max_consolidation_batch()
-    batch_key = set_new_batchid()
-    new_results = collect_new_results_table()
+        scan_for_consolidation()
+        consolidation_key = set_max_consolidation_batch()
+        batch_key = set_new_batchid()
+        new_results = collect_new_results_table()
 
-    if batch_key == consolidation_key:
-        send_tele_sig(str(new_results) + " Consolidation TEST")
+        if batch_key == consolidation_key:
+            send_tele_sig(str(new_results) + " Consolidation TEST")
